d9e0f2821d72c20019e31fa95e2ab751f3def3b0
Julia <juliamakowski@hotmail.com>
2022-06-12 23:23:19 -0300

    fix byte[]

--- src/domain/IncomingReceiverMessage.java
+++ src/domain/IncomingReceiverMessage.java
@@ -3,6 +3,7 @@ package domain;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 public class IncomingReceiverMessage extends Thread {
@@ -15,6 +16,26 @@ public class IncomingReceiverMessage extends Thread {
         this.socket = socket;
     }
 
+    public String removeNull(DatagramPacket packet){
+        byte[] data = packet.getData();
+        String s = new String(packet.getData(), packet.getOffset(), packet.getData().length);
+        int i = 0;
+        char[] c = s.toCharArray();
+        while (i < data.length) {
+            if (packet.getData()[i] != 0) {
+                break;
+            }
+            i++;
+        }
+        byte[] result;
+        if (i > 0 && i < data.length) {
+            result = Arrays.copyOfRange(data, i, data.length);
+        } else {
+            result = data;
+        }
+        return result.toString();
+    }
+
     @Override
     public void run() {
         while (true) {
@@ -22,7 +43,8 @@ public class IncomingReceiverMessage extends Thread {
                 DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
                 System.out.println("Will wait");
                 socket.receive(packet);
-                String payload = new String(packet.getData(), 0, packet.getData().length); // TODO Ju, faz um crop dos elementos null <- ta vindo sujeira aqui...
+                String payload = removeNull(packet);
+                //String payload = new String(packet.getData(), packet.getOffset(), packet.getData().length);
                 handler.accept(new Message(packet.getSocketAddress(), payload));
             } catch (IOException e) {
                 throw new RuntimeException(e);
